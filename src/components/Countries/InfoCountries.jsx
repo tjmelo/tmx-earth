cfd76d49bd06c1d146bd8a9ccac2797a1a5dd756
Thiago Melo <thiago.tmwd@gmail.com>
2022-03-12 11:54:05 -0300

    Improving entry components

--- src/components/Countries/InfoCountries.jsx
+++ src/components/Countries/InfoCountries.jsx
@@ -2,9 +2,10 @@ import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
 
-import { MountListCountries } from './mountListCountries';
+import { MountListCountries } from '../mountListCountries';
+import Loading from '../Load';
 
-export const InfoCountries = props => {
+export const InfoCountries = () => {
 
     const country = useSelector(state => state.country.country);
     const [info, setInfo] = useState([]);
@@ -14,7 +15,7 @@ export const InfoCountries = props => {
         setInfo(<div className="alert alert-primary">Getting data...</div>)
 
         if(country.length === 0 || country === 'Select a country'){
-            setInfo(<div className="alert alert-warning">Select a country!</div>)
+            setInfo(<Loading type='warning'>Select a country!</Loading>)
         }else{
             const data = axios.get(`https://restcountries.com/v3.1/name/${country}?fullText=true`)
             data.then(({data}) => {
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,22 @@
-import React from "react"
+import React, { lazy, Suspense } from "react"
 import ReactDOM from "react-dom"
 import { Provider } from "react-redux"
 import store from "./store/store"
 
 import { ListCoutries } from "./components/ListCoutries"
-import { InfoCountries } from "./components/InfoCountries"
-
+import Loading from "./components/Load"
 import "./global.css"
+
+const InfoCountries = lazy(() => import("./components/Countries"))
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <ListCoutries />
-      <InfoCountries />
+      <Suspense 
+        fallback={<Loading type="info">Loading app!</Loading>}>
+        <InfoCountries />
+      </Suspense>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
