12c8b3161fe275dcd0e0e4eed31efbba5daed1d2
Thiago Melo <thiagojosedemelo@hotmail.com>
2023-04-21 20:53:30 -0300

    Fix bug load performance

--- src/components/ListCountries.tsx
+++ src/components/ListCountries.tsx
@@ -1,19 +1,19 @@
 import React, { useEffect, useState } from 'react'
-import { apiListCoutries } from '../model/api'
+import { apiListCountries } from '../model/api'
 import { useDispatch } from 'react-redux'
 import { update } from '../feature/country/countrySlice'
 import Loading from './Load'
 
-export const ListCoutries = () => {
+export const ListCountries = () => {
   const dispatch = useDispatch()
   const [data, setData] = useState<[]>([])
 
   useEffect(() => {
-    apiListCoutries().then(({ data, status }) => 
+    apiListCountries().then(({ data, status }) => 
       status !== 200 
       ? setData([]) 
       : setData(data))
-  }, [data])
+  }, [])
 
   const selectCountry = (e: { target: { value: any } }) => {
     dispatch(update(e.target.value))
